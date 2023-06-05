e90f8619ffa095eeb57c7a904454a4829f094333
rainiwu <rainiwu01@gmail.com>
2023-06-05 16:25:26 -0700

    load task-specific models, move types to common types submodule

--- prune/models.py
+++ prune/models.py
@@ -1,39 +1,73 @@
-from typing import Dict, Literal, Tuple, Any
+from typing import Dict, Tuple, Any
 
 from transformers import (
     GPT2Tokenizer,
-    GPT2Model,
+    GPT2ForQuestionAnswering,
+    GPT2ForSequenceClassification,
     BertTokenizer,
-    BertModel,
+    BertForQuestionAnswering,
+    BertForSequenceClassification,
     AlbertTokenizer,
-    AlbertModel,
+    AlbertForQuestionAnswering,
+    AlbertForSequenceClassification,
     RobertaTokenizer,
-    RobertaModel,
+    RobertaForQuestionAnswering,
+    RobertaForSequenceClassification,
     ElectraTokenizer,
-    ElectraModel,
+    ElectraForQuestionAnswering,
+    ElectraForSequenceClassification,
 )
 
-ModelType = Literal["gpt2", "bert", "albert", "roberta", "electra"]
+from prune.types import MetricType, ModelType
 
-pretrained: Dict[ModelType, Tuple[Any, Any]] = {
-    "gpt2": (
-        GPT2Tokenizer.from_pretrained("gpt2"),
-        GPT2Model.from_pretrained("gpt2"),
-    ),
-    "bert": (
-        BertTokenizer.from_pretrained("bert-base-uncased"),
-        BertModel.from_pretrained("bert-base-uncased"),
-    ),
-    "albert": (
-        AlbertTokenizer.from_pretrained("albert-base-v2"),
-        AlbertModel.from_pretrained("albert-base-v2"),
-    ),
-    "roberta": (
-        RobertaTokenizer.from_pretrained("roberta-base"),
-        RobertaModel.from_pretrained("roberta-base"),
-    ),
-    "electra": (
-        ElectraTokenizer.from_pretrained("google/electra-base-discriminator"),
-        ElectraModel.from_pretrained("google/electra-base-discriminator"),
-    ),
+
+pretrained: Dict[MetricType, Dict[ModelType, Tuple[Any, Any]]] = {
+    "squad": {
+        "gpt2": (
+            GPT2Tokenizer.from_pretrained("gpt2"),
+            GPT2ForQuestionAnswering.from_pretrained("gpt2"),
+        ),
+        "bert": (
+            BertTokenizer.from_pretrained("bert-base-uncased"),
+            BertForQuestionAnswering.from_pretrained("bert-base-uncased"),
+        ),
+        "albert": (
+            AlbertTokenizer.from_pretrained("albert-base-v2"),
+            AlbertForQuestionAnswering.from_pretrained("albert-base-v2"),
+        ),
+        "roberta": (
+            RobertaTokenizer.from_pretrained("roberta-base"),
+            RobertaForQuestionAnswering.from_pretrained("roberta-base"),
+        ),
+        "electra": (
+            ElectraTokenizer.from_pretrained("google/electra-base-discriminator"),
+            ElectraForQuestionAnswering.from_pretrained(
+                "google/electra-base-discriminator"
+            ),
+        ),
+    },
+    "glue": {
+        "gpt2": (
+            GPT2Tokenizer.from_pretrained("gpt2"),
+            GPT2ForSequenceClassification.from_pretrained("gpt2"),
+        ),
+        "bert": (
+            BertTokenizer.from_pretrained("bert-base-uncased"),
+            BertForSequenceClassification.from_pretrained("bert-base-uncased"),
+        ),
+        "albert": (
+            AlbertTokenizer.from_pretrained("albert-base-v2"),
+            AlbertForSequenceClassification.from_pretrained("albert-base-v2"),
+        ),
+        "roberta": (
+            RobertaTokenizer.from_pretrained("roberta-base"),
+            RobertaForSequenceClassification.from_pretrained("roberta-base"),
+        ),
+        "electra": (
+            ElectraTokenizer.from_pretrained("google/electra-base-discriminator"),
+            ElectraForSequenceClassification.from_pretrained(
+                "google/electra-base-discriminator"
+            ),
+        ),
+    },
 }
---
+++ prune/types.py
@@ -0,0 +1,4 @@
+from typing import Literal
+
+ModelType = Literal["gpt2", "bert", "albert", "roberta", "electra"]
+MetricType = Literal["squad", "glue"]
