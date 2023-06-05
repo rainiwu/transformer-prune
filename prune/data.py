e7163fdff7ae528ad104076d04269086a9d4804e
rainiwu <rainiwu01@gmail.com>
2023-06-05 14:52:59 -0700

    update pretrained model weights

--- prune/data.py
+++ prune/data.py
@@ -16,9 +16,24 @@ from transformers import (
 ModelType = Literal["gpt2", "bert", "albert", "roberta", "electra"]
 
 pretrained: Dict[ModelType, Tuple[Any, Any]] = {
-    "gpt2": (GPT2Tokenizer, GPT2Model),
-    "bert": (BertTokenizer, BertModel),
-    "albert": (AlbertTokenizer, AlbertModel),
-    "roberta": (RobertaTokenizer, RobertaModel),
-    "electra": (ElectraTokenizer, ElectraModel),
+    "gpt2": (
+        GPT2Tokenizer.from_pretrained("gpt2"),
+        GPT2Model.from_pretrained("gpt2"),
+    ),
+    "bert": (
+        BertTokenizer.from_pretrained("bert-base-uncased"),
+        BertModel.from_pretrained("bert-base-uncased"),
+    ),
+    "albert": (
+        AlbertTokenizer.from_pretrained("albert-base-v2"),
+        AlbertModel.from_pretrained("albert-base-v2"),
+    ),
+    "roberta": (
+        RobertaTokenizer.from_pretrained("roberta-base"),
+        RobertaModel.from_pretrained("roberta-base"),
+    ),
+    "electra": (
+        ElectraTokenizer.from_pretrained("google/electra-base-discriminator"),
+        ElectraModel.from_pretrained("google/electra-base-discriminator"),
+    ),
 }
