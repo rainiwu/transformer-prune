f9b6c9735e61faf2d042f7f5ddb718a0d7574f1e
rainiwu <rainiwu01@gmail.com>
2023-06-06 13:53:15 -0700

    update types to use auto models

--- prune/models.py
+++ prune/models.py
@@ -1,71 +1,59 @@
 from typing import Dict, Tuple, Any
 
 from transformers import (
-    GPT2Tokenizer,
-    GPT2ForQuestionAnswering,
-    GPT2ForSequenceClassification,
-    BertTokenizer,
-    BertForQuestionAnswering,
-    BertForSequenceClassification,
-    AlbertTokenizer,
-    AlbertForQuestionAnswering,
-    AlbertForSequenceClassification,
-    RobertaTokenizer,
-    RobertaForQuestionAnswering,
-    RobertaForSequenceClassification,
-    ElectraTokenizer,
-    ElectraForQuestionAnswering,
-    ElectraForSequenceClassification,
+    AutoTokenizer,
+    AutoModelForQuestionAnswering,
+    AutoModelForSequenceClassification,
 )
 
 from prune.types import MetricType, ModelType
 
 
-pretrained: Dict[MetricType, Dict[ModelType, Tuple[Any, Any]]] = {
+PRETRAINED: Dict[MetricType, Dict[ModelType, Tuple[Any, Any]]] = {
     "squad": {
         "gpt2": (
-            GPT2Tokenizer.from_pretrained("gpt2"),
-            GPT2ForQuestionAnswering.from_pretrained("gpt2"),
+            AutoTokenizer.from_pretrained("gpt2"),
+            AutoModelForQuestionAnswering.from_pretrained("gpt2"),
         ),
         "bert": (
-            BertTokenizer.from_pretrained("bert-base-uncased"),
-            BertForQuestionAnswering.from_pretrained("bert-base-uncased"),
+            AutoTokenizer.from_pretrained("bert-base-uncased"),
+            AutoModelForQuestionAnswering.from_pretrained("bert-base-uncased"),
         ),
         "albert": (
-            AlbertTokenizer.from_pretrained("albert-base-v2"),
-            AlbertForQuestionAnswering.from_pretrained("albert-base-v2"),
+            AutoTokenizer.from_pretrained("albert-base-v2"),
+            AutoModelForQuestionAnswering.from_pretrained("albert-base-v2"),
         ),
         "roberta": (
-            RobertaTokenizer.from_pretrained("roberta-base"),
-            RobertaForQuestionAnswering.from_pretrained("roberta-base"),
+            AutoTokenizer.from_pretrained("roberta-base"),
+            AutoModelForQuestionAnswering.from_pretrained("roberta-base"),
         ),
         "electra": (
-            ElectraTokenizer.from_pretrained("google/electra-base-discriminator"),
-            ElectraForQuestionAnswering.from_pretrained(
+            AutoTokenizer.from_pretrained("google/electra-base-discriminator"),
+            AutoModelForQuestionAnswering.from_pretrained(
                 "google/electra-base-discriminator"
             ),
         ),
     },
     "glue": {
         "gpt2": (
-            GPT2Tokenizer.from_pretrained("gpt2"),
-            GPT2ForSequenceClassification.from_pretrained("gpt2"),
+            AutoTokenizer.from_pretrained("gpt2"),
+            AutoModelForSequenceClassification.from_pretrained("gpt2"),
         ),
         "bert": (
-            BertTokenizer.from_pretrained("bert-base-uncased"),
-            BertForSequenceClassification.from_pretrained("bert-base-uncased"),
+            AutoTokenizer.from_pretrained("bert-base-uncased"),
+            AutoModelForSequenceClassification.from_pretrained("bert-base-uncased"),
         ),
         "albert": (
-            AlbertTokenizer.from_pretrained("albert-base-v2"),
-            AlbertForSequenceClassification.from_pretrained("albert-base-v2"),
+            AutoTokenizer.from_pretrained("albert-base-v2"),
+            AutoModelForSequenceClassification.from_pretrained("albert-base-v2"),
         ),
         "roberta": (
-            RobertaTokenizer.from_pretrained("roberta-base"),
-            RobertaForSequenceClassification.from_pretrained("roberta-base"),
+            AutoTokenizer.from_pretrained("roberta-base"),
+            AutoModelForSequenceClassification.from_pretrained("roberta-base"),
         ),
         "electra": (
-            ElectraTokenizer.from_pretrained("google/electra-base-discriminator"),
-            ElectraForSequenceClassification.from_pretrained(
+            AutoTokenizer.from_pretrained("google/electra-base-discriminator"),
+            AutoModelForSequenceClassification.from_pretrained(
                 "google/electra-base-discriminator"
             ),
         ),
