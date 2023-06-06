3c68aae37c16c50a345e898bdd6c1a4daf78f4c9
rainiwu <rainiwu01@gmail.com>
2023-06-06 15:35:57 -0700

    initial metrics submodule implementation

---
+++ prune/metrics.py
@@ -0,0 +1,216 @@
+"""
+submodule defining operations on metric datasets
+
+significant portions of this submodule are adapated from
+https://huggingface.co/learn/nlp-course/
+with modifications for integration purposes
+
+the huggingface nlp course is licensed under the Apache v2.0 license
+you may obtain a copy of the license here http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from typing import Callable
+import collections
+import functools
+
+import numpy as np
+import datasets
+import evaluate
+from tqdm.auto import tqdm
+import torch
+from torch.utils.data import DataLoader
+from transformers import default_data_collator
+
+SQUAD_DATA = datasets.load_dataset("squad")
+METRIC = evaluate.load("squad")
+
+
+def preprocess_training_examples(examples, tokenizer: Callable):
+    """
+    training set preprocessing adapted from huggingface course:
+    https://huggingface.co/learn/nlp-course/chapter7/7
+    """
+    max_length = 384
+    stride = 128
+
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=max_length,
+        truncation="only_second",
+        stride=stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    sample_map = inputs.pop("overflow_to_sample_mapping")
+    answers = examples["answers"]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        sample_idx = sample_map[i]
+        answer = answers[sample_idx]
+        start_char = answer["answer_start"][0]
+        end_char = answer["answer_start"][0] + len(answer["text"][0])
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end of the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # If the answer is not fully inside the context, label is (0, 0)
+        if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return inputs
+
+
+def preprocess_validation_examples(examples, tokenizer: Callable):
+    """
+    validation set preprocessing adapted from huggingface course:
+    https://huggingface.co/learn/nlp-course/chapter7/7
+    """
+    max_length = 384
+    stride = 128
+
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=max_length,
+        truncation="only_second",
+        stride=stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    sample_map = inputs.pop("overflow_to_sample_mapping")
+    example_ids = []
+
+    for i in range(len(inputs["input_ids"])):
+        sample_idx = sample_map[i]
+        example_ids.append(examples["id"][sample_idx])
+
+        sequence_ids = inputs.sequence_ids(i)
+        offset = inputs["offset_mapping"][i]
+        inputs["offset_mapping"][i] = [
+            o if sequence_ids[k] == 1 else None for k, o in enumerate(offset)
+        ]
+
+    inputs["example_id"] = example_ids
+    return inputs
+
+
+def compute_metrics(start_logits, end_logits, features, examples):
+    """
+    metric computation adapted from huggingface course
+    """
+    n_best = 20
+    max_answer_length = 30
+
+    example_to_features = collections.defaultdict(list)
+    for idx, feature in enumerate(features):
+        example_to_features[feature["example_id"]].append(idx)
+
+    predicted_answers = []
+    for example in tqdm(examples):
+        example_id = example["id"]
+        context = example["context"]
+        answers = []
+
+        # Loop through all features associated with that example
+        for feature_index in example_to_features[example_id]:
+            start_logit = start_logits[feature_index]
+            end_logit = end_logits[feature_index]
+            offsets = features[feature_index]["offset_mapping"]
+
+            start_indexes = np.argsort(start_logit)[-1 : -n_best - 1 : -1].tolist()
+            end_indexes = np.argsort(end_logit)[-1 : -n_best - 1 : -1].tolist()
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    # Skip answers that are not fully in the context
+                    if offsets[start_index] is None or offsets[end_index] is None:
+                        continue
+                    # Skip answers with a length that is either < 0 or > max_answer_length
+                    if (
+                        end_index < start_index
+                        or end_index - start_index + 1 > max_answer_length
+                    ):
+                        continue
+
+                    answer = {
+                        "text": context[
+                            offsets[start_index][0] : offsets[end_index][1]
+                        ],
+                        "logit_score": start_logit[start_index] + end_logit[end_index],
+                    }
+                    answers.append(answer)
+
+        # Select the answer with the best score
+        if len(answers) > 0:
+            best_answer = max(answers, key=lambda x: x["logit_score"])
+            predicted_answers.append(
+                {"id": example_id, "prediction_text": best_answer["text"]}
+            )
+        else:
+            predicted_answers.append({"id": example_id, "prediction_text": ""})
+
+    theoretical_answers = [
+        {"id": ex["id"], "answers": ex["answers"]} for ex in examples
+    ]
+    return METRIC.compute(predictions=predicted_answers, references=theoretical_answers)
+
+
+def finetune_squad() -> torch.nn.Module:
+    """
+    finetune a given model on the squad dataset
+    """
+    train_dataset = SQUAD_DATA["train"].map(  # type: ignore
+        preprocess_training_examples,
+        batched=True,
+        remove_columns=SQUAD_DATA["train"].column_names,  # type: ignore
+    )
+
+    validation_dataset = SQUAD_DATA["validation"].map(  # type: ignore
+        preprocess_validation_examples,
+        batched=True,
+        remove_columns=SQUAD_DATA["validation"].column_names,  # type: ignore
+    )
+
+    train_dataset.set_format("torch")
+    validation_set = validation_dataset.remove_columns(["example_id", "offset_mapping"])
+    validation_set.set_format("torch")
+
+    train_dataloader = DataLoader(
+        train_dataset,  # type: ignore
+        shuffle=True,
+        collate_fn=default_data_collator,
+        batch_size=8,
+    )
+    eval_dataloader = DataLoader(
+        validation_set, collate_fn=default_data_collator, batch_size=8  # type: ignore
+    )
