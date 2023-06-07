fa6ee59ef7f9bdea7d460337d5a6189945c2bfc8
rainiwu <rainiwu01@gmail.com>
2023-06-07 15:56:35 -0700

    add train submodule test import

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,3 +5,7 @@ Test importing prune submodules
 
 def test_import_models() -> None:
     import prune.models  # pyright: ignore
+
+
+def test_import_train() -> None:
+    import prune.train  # pyright: ignore
