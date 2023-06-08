81035631f62a2b8265a9221da6abf3f58676cd02
rainiwu <rainiwu01@gmail.com>
2023-06-08 00:56:39 -0700

    extract model parameters correctly in default pruner submodule

--- prune/pruners/default.py
+++ prune/pruners/default.py
@@ -1,18 +1,25 @@
 """
 performs pruning using built-in torch methods
 """
+from typing import List, Tuple
 
 import torch
 from torch.nn.utils import prune
 
 
 def random_prune(model: torch.nn.Module, sparsity: float) -> None:
+    parameters: List[Tuple[torch.nn.Module, str]] = []
+    for name, module in model.named_modules():
+        parameters.append((module, name))
     prune.global_unstructured(
-        model.parameters(), pruning_method=prune.RandomUnstructured, amount=sparsity
+        parameters, pruning_method=prune.RandomUnstructured, amount=sparsity
     )
 
 
 def lowest_prune(model: torch.nn.Module, sparsity: float) -> None:
+    parameters: List[Tuple[torch.nn.Module, str]] = []
+    for name, module in model.named_modules():
+        parameters.append((module, name))
     prune.global_unstructured(
-        model.parameters(), pruning_method=prune.L1Unstructured, amount=sparsity
+        parameters, pruning_method=prune.L1Unstructured, amount=sparsity
     )
